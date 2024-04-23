649f266271f9ef9dcc52621345ed05d8f25a8051
Jane <jane@occam.fi>
2024-04-23 14:19:04 +0300

    add headers

--- src/shared/graphql/client.ts
+++ src/shared/graphql/client.ts
@@ -2,5 +2,10 @@ import { getSdk } from "shared/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const endpoint = `${import.meta.env.VITE_GRAPHQL_SERVER}/query` ?? "";
-const client = new GraphQLClient(endpoint);
+
+const client = new GraphQLClient(endpoint, {
+  credentials: "include",
+  mode: "cors",
+});
+
 export const graphqlSdk = getSdk(client);
